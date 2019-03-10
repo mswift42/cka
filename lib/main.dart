2d5721960548e7b521f01275e7b34fb38d129992
mswift42 <martin.haesler@gmail.com>
2019-03-10 11:03:56 +0000

    add ThemeData.

--- lib/main.dart
+++ lib/main.dart
@@ -5,4 +5,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CK',
+      theme: ThemeData(
+        primaryColor: Colors.green[500],
+        accentColor: Colors.lime[500],
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: '/',
+    );
+  }
 }
