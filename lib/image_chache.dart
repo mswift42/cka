77aa1aaa789466f10c7e68ed9798da7d28d41607
mswift42 <martin.haesler@gmail.com>
2019-04-01 14:30:46 +0100

    add method cachedImage.

--- lib/image_chache.dart
+++ lib/image_chache.dart
@@ -1,11 +1,17 @@
 import 'dart:async';
 import 'dart:io' show File;
 
+import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 class ImageCache {
-  Future<File> getImageFile(String imageUrl) async {
+  Future<File> _getImageFile(String imageUrl) async {
     var file = await DefaultCacheManager().getSingleFile(imageUrl);
     return file;
+  }
+
+  Future<Image> cachedImage(String imageUrl) async {
+    var file = await _getImageFile(imageUrl);
+    return Image.file(file);
   }
 }
