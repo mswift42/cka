a8315671a1ef3652aca56035beb50864b0e0d44e
mswift42 <martin.haesler@gmail.com>
2019-04-01 14:21:01 +0100

    rename to getImageFile.

--- lib/image_chache.dart
+++ lib/image_chache.dart
@@ -4,7 +4,7 @@ import 'dart:io' show File;
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 class ImageCache {
-  Future<File> findPath(String imageUrl) async {
+  Future<File> getImageFile(String imageUrl) async {
     var file = await DefaultCacheManager().getSingleFile(imageUrl);
     return file;
   }
