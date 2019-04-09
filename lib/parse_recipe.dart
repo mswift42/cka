74d3535523b80592377e349d4506de15a2404a83
mswift42 <martin.haesler@gmail.com>
2019-04-09 06:09:18 +0100

    add method getCKPage.

--- lib/parse_recipe.dart
+++ lib/parse_recipe.dart
@@ -1,6 +1,19 @@
-import 'package:cka/Recipe.dart';
+import 'package:http/http.dart' as http;
 
+const CKPrefix = 'https://www.chefkoch.de';
 
 class ParseRecipes {
   String searchterm;
+  String page;
+
+  ParseRecipes(this.searchterm, this.page);
+
+  Future<String> getCKPage() async {
+    http.Response response = await http.get(queryUrl());
+    return response.body;
+  }
+
+  String queryUrl() {
+    return '$CKPrefix/rs/s$page/$searchterm/Rezepte.html#more2';
+  }
 }
