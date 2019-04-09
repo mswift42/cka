1ee20cd8c35389129f1b3f196df2b5670ace9169
mswift42 <martin.haesler@gmail.com>
2019-04-09 06:30:41 +0100

    add class CKDocument.

--- lib/parse_recipe.dart
+++ lib/parse_recipe.dart
@@ -1,12 +1,14 @@
+import 'package:html/dom.dart';
+import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 
 const CKPrefix = 'https://www.chefkoch.de';
 
-class ParseRecipes {
+class CKBody {
   String searchterm;
   String page;
 
-  ParseRecipes(this.searchterm, this.page);
+  CKBody(this.searchterm, this.page);
 
   Future<String> getCKPage() async {
     http.Response response = await http.get(queryUrl());
@@ -16,4 +18,18 @@ class ParseRecipes {
   String queryUrl() {
     return '$CKPrefix/rs/s$page/$searchterm/Rezepte.html#more2';
   }
+}
+
+class CKDocument {
+  String searchterm;
+  String page;
+
+  CKDocument(this.searchterm, this.page);
+
+  Future<Document> getDoc() async {
+    ck = new CKBody(searchterm, page);
+    ckbody = await ck.getCKPage();
+    return ckbody;
+  }
+
 }
