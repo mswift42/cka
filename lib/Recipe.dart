1dc3bea1bec8dba8935af9000f0e951b0388f89b
mswift42 <martin.haesler@gmail.com>
2019-04-11 06:04:41 +0100

    move parse_recipe into Recipe.dart

--- lib/Recipe.dart
+++ lib/Recipe.dart
@@ -1,3 +1,7 @@
+import 'package:html/dom.dart';
+import 'package:html/parser.dart';
+import 'package:http/http.dart' as http;
+
 class Recipe {
   String title;
   String subtitle;
@@ -67,4 +71,41 @@ class SearchQuery {
   int page;
 
   SearchQuery(this.searchterm, this.page);
+}
+
+const CKPrefix = 'https://www.chefkoch.de';
+
+class CKBody {
+  String searchterm;
+  String page;
+
+  CKBody(this.searchterm, this.page);
+
+  Future<String> getCKPage() async {
+    http.Response response = await http.get(queryUrl());
+    return response.body;
+  }
+
+  String queryUrl() {
+    return '$CKPrefix/rs/s$page/$searchterm/Rezepte.html#more2';
+  }
+}
+
+class CKDocument {
+  String searchterm;
+  String page;
+
+  CKDocument(this.searchterm, this.page);
+
+  Future<Document> getDoc() async {
+    CKBody ck = CKBody(searchterm, page);
+    String ckbody = await ck.getCKPage();
+    return parse(ckbody);
+  }
+}
+
+class CKDocSelection {
+  Node cknode;
+
+  CKDocSelection(this.cknode);
 }
