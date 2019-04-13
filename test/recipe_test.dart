5d8a81d5e7c9aff9f34c0b91c2d0f23b5517ca41
mswift42 <martin.haesler@gmail.com>
2019-04-13 11:33:18 +0100

    add recipe_test

--- test/recipe_test.dart
+++ test/recipe_test.dart
@@ -1,1 +1,16 @@
-void main() {}
+import 'dart:io';
+
+import 'package:cka/recipe.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:html/parser.dart';
+
+void main() {
+  test('parse ck results page', () {
+    File file = File('testhtml/bohnen.html');
+    file.readAsString().then((String contents) {
+      var body = parse(contents);
+      var ckdocsel = CKDocSelection(body.querySelector('.search-list-item'));
+      expect(ckdocsel.title(), 'Grüne Bohnen im Speckmantel');
+    });
+  });
+}
