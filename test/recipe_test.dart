8974bda1a36bf2a04045381c3a1eee22008a8874
mswift42 <martin.haesler@gmail.com>
2019-04-16 06:18:49 +0100

    use readAsStringSync to test local html.

--- test/recipe_test.dart
+++ test/recipe_test.dart
@@ -6,21 +6,20 @@ import 'package:test/test.dart';
 
 void main() {
   test('parse ck results page', () {
-    File file = File('testhtml/bohnen.html');
-    file.readAsString().then((String contents) {
-      var body = parse(contents);
-      var ckdocsel = CKDocSelection(body.querySelector('.search-list-item'));
-      expect(ckdocsel.title(), 'Grüne Bohnen im Speckmantel');
-      expect(ckdocsel.subtitle(),
-          "Bohnen waschen und die Spitzen abschneiden. Bohnenkraut, Knoblauch, zerdrückte Pfefferkörner und Salz mit Öl kurz anrösten. 2 Lite...");
-      expect(ckdocsel.url(),
-          'https://www.chefkoch.de/rezepte/563451154612271/Gruene-Bohnen-im-Speckmantel.html');
-      expect(ckdocsel.thumbnail(),
-          'https://static.chefkoch-cdn.de/rs/bilder/56345/gruene-bohnen-im-speckmantel-1124631-150x150.jpg');
-      expect(ckdocsel.rating(), '4.49');
-      expect(ckdocsel.difficulty(), 'simpel');
-      expect(ckdocsel.preptime(), '30 min.');
-    });
+    File file = File('test/testhtml/bohnen.html');
+    var contents = file.readAsStringSync();
+    var body = parse(contents);
+    var ckdocsel = CKDocSelection(body.querySelector('.search-list-item'));
+    expect(ckdocsel.title(), 'Grüne Bohnen im Speckmantel');
+    expect(ckdocsel.subtitle(),
+        "Bohnen waschen und die Spitzen abschneiden. Bohnenkraut, Knoblauch, zerdrückte Pfefferkörner und Salz mit Öl kurz anrösten. 2 Lite...");
+    expect(ckdocsel.url(),
+        'https://www.chefkoch.de/rezepte/563451154612271/Gruene-Bohnen-im-Speckmantel.html');
+    expect(ckdocsel.thumbnail(),
+        'https://static.chefkoch-cdn.de/rs/bilder/56345/gruene-bohnen-im-speckmantel-1124631-150x150.jpg');
+    expect(ckdocsel.rating(), '4.49');
+    expect(ckdocsel.difficulty(), 'simpel');
+    expect(ckdocsel.preptime(), '30 min.');
   });
 
   test('parse Recipe detail page', () {
