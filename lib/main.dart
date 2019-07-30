5b5272af076fbd0b2204b29f0739539e035512d1
mswift42 <martin.haesler@gmail.com>
2019-07-30 15:43:45 +0100

    add lastsearchwidget / grid.

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,8 @@ class MyApp extends StatelessWidget {
 }
 
 class RecipeSearch extends StatefulWidget {
-  final  LastSearchService searchService = LastSearchService();
+  final LastSearchService searchService = LastSearchService();
+
   @override
   _RecipeSearchState createState() => _RecipeSearchState();
 }
@@ -516,6 +517,63 @@ class _IngredientsLine extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class LastSearchGrid extends StatelessWidget {
+  final ValueChanged<String> onDeleted;
+  final ValueChanged<String> onTapped;
+  final List<String> _lastSearches;
+
+  LastSearchGrid(this.onDeleted, this.onTapped, this._lastSearches);
+
+  void _handleOnDelete(String value) {
+    onDeleted(value);
+  }
+
+  void _handleOnTap(String value) {
+    onTapped(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Wrap(
+      spacing: 12.0,
+      runSpacing: 4.0,
+      children: _lastSearches
+          .map((i) => LastSearchWidget(i, _handleOnDelete, _handleOnTap))
+          .toList(),
+    ));
+  }
+}
+class LastSearchWidget extends StatelessWidget {
+  final String value;
+  final ValueChanged<String> onDeleted;
+  final ValueChanged<String> onTapped;
+
+  LastSearchWidget(this.value, this.onDeleted, this.onTapped);
+
+  void _handleOnDelete() {
+    onDeleted(value);
+  }
+
+  void _handleTap() {
+    onTapped(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Chip(
+        label: GestureDetector(
+          child: Text(value),
+          onTap: _handleTap,
+        ),
+        deleteIcon: Icon(Icons.delete),
+        onDeleted: _handleOnDelete,
       ),
     );
   }
