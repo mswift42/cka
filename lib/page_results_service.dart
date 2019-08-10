6dc2c29b7a7b7f8af004603039053b197da32b2f
mswift42 <martin.haesler@gmail.com>
2019-08-10 15:10:23 +0100

    move nextpage / prevPage to pageresultsservice.

--- lib/page_results_service.dart
+++ lib/page_results_service.dart
@@ -1,3 +1,23 @@
 class PageResultsService {
+  int currentPage;
 
+
+  String nextPage(String currentPage) {
+    var curr = int.tryParse(currentPage);
+    if (curr == null) {
+      return "";
+    }
+    return (curr + 30).toString();
+  }
+
+  String prevPage(String currentPage) {
+    var curr = int.tryParse(currentPage);
+    if (curr == null) {
+      return "";
+    }
+    if (curr < 30) {
+      return "0";
+    }
+    return (curr - 30).toString();
+  }
 }
--- lib/recipe.dart
+++ lib/recipe.dart
@@ -78,24 +78,6 @@ class SearchQuery {
   SearchQuery(this.searchterm, this.page);
 }
 
-String nextPage(String currentPage) {
-  var curr = int.tryParse(currentPage);
-  if (curr == null) {
-    return "";
-  }
-  return (curr + 30).toString();
-}
-
-String prevPage(String currentPage) {
-  var curr = int.tryParse(currentPage);
-  if (curr == null) {
-    return "";
-  }
-  if (curr < 30) {
-    return "0";
-  }
-  return (curr - 30).toString();
-}
 
 const CKPrefix = 'https://www.chefkoch.de';
 
