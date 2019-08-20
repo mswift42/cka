16d49eacb52a63d1446341997548fdec21d07766
mswift42 <martin.haesler@gmail.com>
2019-08-20 15:46:46 +0100

    delete prevPage method.

--- lib/page_results_service.dart
+++ lib/page_results_service.dart
@@ -10,14 +10,4 @@ class PageResultsService {
     return (curr + 30).toString();
   }
 
-  String prevPage(String currentPage) {
-    var curr = int.tryParse(currentPage);
-    if (curr == null) {
-      return "";
-    }
-    if (curr < 30) {
-      return "0";
-    }
-    return (curr - 30).toString();
-  }
 }
