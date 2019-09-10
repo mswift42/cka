4a3c8a028d605c480453ff364fa0b4f8e988c109
mswift42 <martin.haesler@gmail.com>
2019-09-10 05:56:11 +0100

    add class FavouritesView.

---
+++ lib/fvaourites_view.dart
@@ -0,0 +1,15 @@
+import 'package:cka/recipe.dart';
+import 'package:flutter/material.dart';
+
+class FavouritesView extends StatefulWidget {
+  @override
+  _FavouritesViewState createState() => _FavouritesViewState();
+}
+
+class _FavouritesViewState extends State<FavouritesView> {
+  List<RecipeDetail> _favourites = [];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
