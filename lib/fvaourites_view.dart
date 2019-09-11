64dd522295e63cb14279a99d2b3d95eefc154290
mswift42 <martin.haesler@gmail.com>
2019-09-11 05:58:00 +0100

    add gridTileBar for favouriteView.

--- lib/fvaourites_view.dart
+++ lib/fvaourites_view.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cka/recipe.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class FavouritesView extends StatefulWidget {
   final List<RecipeDetail> favourites;
@@ -11,16 +13,16 @@ class FavouritesView extends StatefulWidget {
 }
 
 class _FavouritesViewState extends State<FavouritesView> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Favoriten"),
       ),
-      body: GridView.extent(maxCrossAxisExtent: 480.00,
+      body: GridView.extent(
+        maxCrossAxisExtent: 480.00,
         children:
-        widget.favourites.map((i) => Favourite(recipeDetail: i)).toList(),
+            widget.favourites.map((i) => Favourite(recipeDetail: i)).toList(),
       ),
     );
   }
@@ -36,9 +38,31 @@ class Favourite extends StatefulWidget {
 }
 
 class _FavouriteState extends State<Favourite> {
+  ImageProvider image;
+
+  @override
+  void initState() {
+    super.initState();
+    image = CachedNetworkImageProvider(widget.recipeDetail.thumbnail);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: () => _showFavourite(context),
+      child: GridTile(
+        child: FadeInImage(
+          placeholder: MemoryImage(kTransparentImage),
+          image: image,
+          fit: BoxFit.fitWidth,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          title: Text(widget.recipeDetail.title),
+          subtitle: Text(
+              "Difficulty :${widget.recipeDetail.difficulty} ,Cooking Time: ${widget.recipeDetail.cookingtime} "),
+        ),
+      ),
+    );
   }
 }
-
