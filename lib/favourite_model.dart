ca6e63143c9d4e55c58f6b24c224ab67c033941e
mswift42 <martin.haesler@gmail.com>
2019-09-14 11:50:50 +0100

    add deleteMethod / getter.

--- lib/favourite_model.dart
+++ lib/favourite_model.dart
@@ -1,4 +1,16 @@
+import 'dart:collection';
+
 import 'package:cka/recipe.dart' show RecipeDetail;
+import 'package:flutter/material.dart';
+
 class FavouriteModel extends ChangeNotifier {
   final List<RecipeDetail> _favourites = [];
+
+  UnmodifiableListView<RecipeDetail> get items =>
+      UnmodifiableListView(_favourites);
+
+  void delete(RecipeDetail favourite) {
+    _favourites.remove(favourite);
+    notifyListeners();
+  }
 }
