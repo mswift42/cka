8a4b585407452deec8ae767a4e50e34baf59a16a
mswift42 <martin.haesler@gmail.com>
2019-09-16 14:29:27 +0100

    check for title of app.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,9 +12,7 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(CKApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsNothing);
+    final titleFinder = find.text('CK');
+    expect(titleFinder, findsOneWidget);
   });
 }
