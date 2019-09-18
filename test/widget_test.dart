821e1c1eea378e3ad1000b6ec169cc3fb1f80a99
mswift42 <martin.haesler@gmail.com>
2019-09-18 05:44:37 +0100

    test search textfield.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,13 +6,26 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:cka/main.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('test correct title in app', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(CKApp());
     final titleFinder = find.text('CK');
     expect(titleFinder, findsOneWidget);
+  });
+
+  testWidgets('search filters have a label', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: RecipeSearch(),
+    ));
+    final searchFilterFinder = find.text('relevanz');
+    final searchFilterFinder2 = find.text('bewertung');
+    expect(searchFilterFinder, findsOneWidget);
+    expect(searchFilterFinder2, findsOneWidget);
+    final textFieldFinder = find.byType(TextField);
+    expect(textFieldFinder, findsOneWidget);
   });
 }
