26a26670bb9cd3095e9e49a6462e738b7cfefcf8
mswift42 <martin.haesler@gmail.com>
2019-09-26 05:22:46 +0100

    add add / remove methods.

--- lib/models/lastsearch.dart
+++ lib/models/lastsearch.dart
@@ -6,9 +6,21 @@ import 'package:flutter/material.dart';
 
 class LastSearchModel extends ChangeNotifier {
   List<String> _savedSearches = [];
-  void _lastSearches() async {
+  void lastSearches() async {
     var searchService = LastSearchService();
     var searches = await searchService.readSearches();
     _savedSearches = searches;
+  }
+
+  List<String> get searches => _savedSearches;
+
+  void add(String search) {
+    _savedSearches.add(search);
+    notifyListeners();
+  }
+
+  void remove(String search) {
+    _savedSearches.remove(search);
+    notifyListeners();
   }
 }
