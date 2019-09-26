d850fd062dbcbd7703274bee751b9ec001a6778a
mswift42 <martin.haesler@gmail.com>
2019-09-26 05:03:35 +0100

    await readSearches.

--- lib/models/lastsearch.dart
+++ lib/models/lastsearch.dart
@@ -1,36 +1,14 @@
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
 import 'dart:async';
 
+import 'package:cka/last_search_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class LastSearchService extends ChangeNotifier {
-  final String lastSearchesFile = 'lastsearches.txt';
-
-  Future<String> get _localPath async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
-  }
-
-  Future<File> get _localFile async {
-    final path = await _localPath;
-    return File('$path/$lastSearchesFile');
-  }
-
-  Future<File> writeSearches(List<String> searches) async {
-    final file = await _localFile;
-    return file.writeAsString(searches.join(','));
-  }
-
-  Future<List<String>> readSearches() async {
-    try {
-      final file = await _localFile;
-
-      String contents = await file.readAsString();
-      return contents.split(',');
-    } catch (e) {
-      print(e);
-    }
-    return null;
+class LastSearchModel extends ChangeNotifier {
+  List<String> _savedSearches = [];
+  void _lastSearches() async {
+    var searchService = LastSearchService();
+    var searches = await searchService.readSearches();
+    _savedSearches = searches;
   }
 }
