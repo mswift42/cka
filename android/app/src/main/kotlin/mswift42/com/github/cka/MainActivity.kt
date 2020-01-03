bd5f674a8a1c9ee5b5e26ffcf5bee642c80cbd19
mswift42 <martin.haesler@gmail.com>
2020-01-03 06:45:57 +0000

    add flutter created folders.

---
+++ android/app/src/main/kotlin/mswift42/com/github/cka/MainActivity.kt
@@ -0,0 +1,12 @@
+package mswift42.com.github.cka
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
